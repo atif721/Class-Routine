dff0dc354c5786320b9def8c42c84188c0201278
atif721 <abdullahatif721@outlook.com>
2026-04-16 11:25:24 +0600

    RoutineTable created and completed

--- src/components/RoutineTable.tsx
+++ src/components/RoutineTable.tsx
@@ -1,14 +1,37 @@
 import type { WeekklySchedule } from "@/utils/types";
 
 interface RoutineTableProps {
-  data: WeekklySchedule;
+  data: WeekklySchedule | null;
   section: string;
 }
 
-const RoutineTable = () => {
+const RoutineTable = ({ data, section }: RoutineTableProps) => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
   return (
     <div className="">
-      <h1>RoutineTable</h1>
+      {days.map((day) => (
+        <div key={day}>
+          <h2>{day}</h2>
+          {data &&
+            data[day] &&
+            Object.entries(data[day]).map(([time, classes]) => (
+              <div key={time}>
+                <p>{time}</p>
+                {classes &&
+                  classes
+                    .filter((cls) => cls.section === section)
+                    .map((cls, i) => (
+                      <div key={i}>
+                        <p> {cls.course} </p>
+                        <p> {cls.teacher_name} </p>
+                        <p> {cls.section} </p>
+                        <p> {cls.room} </p>
+                      </div>
+                    ))}
+              </div>
+            ))}
+        </div>
+      ))}
     </div>
   );
 };
