1ddfb73e47f503508c44a265bced6879c9cde04a
atif721 <abdullahatif721@outlook.com>
2026-04-17 10:59:39 +0600

    FilterBar and Package files updated

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,23 +1,37 @@
 import SemesterSection from "./SemesterSection";
-
 interface FilterBarProps {
   semester: string;
   section: string;
   sections: string[];
+  settings: boolean;
   onSemesterChange: (val: string) => void;
   onSectionChange: (val: string) => void;
+  onSettingsSelect: (val: boolean) => void;
 }
 
-const FilterBar = ({ semester, section, sections, onSemesterChange, onSectionChange }: FilterBarProps) => {
+const FilterBar = ({
+  semester,
+  section,
+  sections,
+  settings,
+  onSemesterChange,
+  onSectionChange,
+  onSettingsSelect,
+}: FilterBarProps) => {
   return (
     <>
-      <SemesterSection
-        semester={semester}
-        section={section}
-        sections={sections}
-        onSemesterChange={onSemesterChange}
-        onSectionChange={onSectionChange}
-      />
+      {settings && (
+        <div className="flex flex-col items-center gap-4 pt-4 min-h-125">
+          <SemesterSection
+            semester={semester}
+            section={section}
+            sections={sections}
+            onSemesterChange={onSemesterChange}
+            onSectionChange={onSectionChange}
+            onSettingsSelect={onSettingsSelect}
+          />
+        </div>
+      )}
     </>
   );
 };
