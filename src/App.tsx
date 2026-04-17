bc1991be993cc3d4d98c6167d02f48234b2f6bbe
atif721 <abdullahatif721@outlook.com>
2026-04-17 11:01:48 +0600

    App component updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import "./App.css";
 import useSheetData from "./hooks/useSheetData";
 import FilterBar from "./components/FilterBar";
 import RoutineTable from "./components/RoutineTable";
+import FloatingRefresh from "./components/FloatingRefresh";
+import Header from "./components/Header";
 
 const App = () => {
   const [semester, setSemester] = useState("");
   const [section, setSection] = useState("");
+  const [settings, setSettings] = useState(true);
 
-  const { data, loading, error, sections = [] } = useSheetData(semester, section);
-
-  if (loading) {
-    return <p>Loading....</p>;
-  }
+  const { data, loading, error, sections = [], refresh } = useSheetData(semester, section);
 
   if (error) {
     return <p>Error Loading....{error}</p>;
@@ -20,14 +19,22 @@ const App = () => {
 
   return (
     <div className="max-w-2xl mx-auto px-4">
+      <Header settings={settings} loading={loading} onSettingsSelect={setSettings} />
       <FilterBar
         semester={semester}
         section={section}
         sections={sections}
+        settings={settings}
+        onSettingsSelect={setSettings}
         onSemesterChange={setSemester}
         onSectionChange={setSection}
       />
-      <RoutineTable data={data} section={section} />
+      {!settings && (
+        <>
+          <RoutineTable data={data} section={section} semester={semester} />
+          <FloatingRefresh loading={loading} onRefresh={refresh} />
+        </>
+      )}
     </div>
   );
 };
