51a61b7cba5419d970a2a8e47d9f8fc919c11bfd
atif721 <abdullahatif721@outlook.com>
2026-04-18 11:09:52 +0600

    package and vite.coonfigs updated

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
       ]
     }
   })],
+  base:process.env.VITE_BASE_PATH || "/Class-Routine",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
