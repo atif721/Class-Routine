1d739039c3fe1983903d68ff8a972bfd31bd5b1d
atif721 <abdullahatif721@outlook.com>
2026-04-18 14:20:03 +0600

    manifest.json file configured

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from "./public/manifest.json" with { type: "json" };
+import type { ManifestOptions } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
-    manifest: {
-      name: 'CSE Class Routine',
-      short_name: 'Routine',
-      description: 'VU CSE Class Routine App',
-      theme_color: '#ffffff',
-      background_color: '#ffffff',
-      display: 'standalone',
-      start_url: '/',
-      icons: [
-        {
-          src: '/icon-192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: '/icon-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    }
+    manifest: manifest as ManifestOptions,
   })],
   base:process.env.VITE_BASE_PATH || "/Class-Routine",
   resolve: {
