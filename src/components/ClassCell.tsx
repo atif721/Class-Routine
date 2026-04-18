e8ba5872ba58a12ed749acadb957a0fc37870487
atif721 <abdullahatif721@outlook.com>
2026-04-18 13:48:04 +0600

    dark mode settings updated

--- src/components/ClassCell.tsx
+++ src/components/ClassCell.tsx
@@ -9,11 +9,11 @@ const ClassCell = ({ time, classes }: ClassCellProps) => {
   return (
     <div className="flex gap-3 items-start mb-3">
       <p className="text-md font-semibold w-20 shrink-0">{time}</p>
-      <div className="flex-1 bg-blue-100 rounded-lg p-3 dark:bg-muted/50">
+      <div className="flex-1 bg-blue-100 rounded-lg p-3 dark:bg-gray-900">
         {classes.map((cls, i) => (
           <div key={i} className="flex flex-col gap-2">
             <p className="font-bold text-lg text-foreground">{cls.course}</p>
-            <p className="text-xs">{cls.teacher_name}</p>
+            <p className="text-sm">{cls.teacher_name}</p>
             <p className="text-md font-semibold text-blue-500">Room: {cls.room}</p>
           </div>
         ))}
