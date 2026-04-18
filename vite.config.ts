d917bce3c415e665c9f1d1c2f7231013788993ac
atif721 <abdullahatif721@outlook.com>
2026-04-18 07:50:30 +0600

    new packages added

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,34 @@ import path from "path";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'CSE Class Routine',
+      short_name: 'Routine',
+      description: 'VU CSE Class Routine App',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url: '/',
+      icons: [
+        {
+          src: '/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
