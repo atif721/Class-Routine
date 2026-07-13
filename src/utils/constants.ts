82bd4264e0ec004ab46b1b762c3168e2b0a09f73
atif721 <abdullahatif721@outlook.com>
2026-07-13 18:32:01 +0600

    semester reduced to 8

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://vuroutine.vercel.app";
-export const SEMESTERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+export const SEMESTERS = ["1", "2", "3", "4", "5", "6", "7", "8"];
