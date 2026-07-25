4f188fc7ea2eb83ba9330c83b9c2468a341ac92d
atif721 <abdullahatif721@outlook.com>
2026-07-25 22:02:58 +0600

    types update changed

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,12 @@
 export interface ClassInfo {
-  teacher_name: string,
-  designation: string,
-  course: string,
-  course_name: string,
-  section: string,
-  room: string,
+  teacher_name: string;
+  designation: string;
+  course: string;
+  course_name: string;
+  section: string;
+  room: string;
+  end_time: string;
 }
 
-export type DaySchedule = Record<string, ClassInfo[] | null>;
+export type DaySchedule = Record<string, ClassInfo | null>;
 export type WeekklySchedule = Record<string, DaySchedule>;
